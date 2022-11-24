964444c06ea3e08566e49fd3b76ed4fe7839d446
RebelRadioYT <119007837+RebelRadioYT@users.noreply.github.com>
2022-11-24 12:29:17 -0700

    Add files via upload

---
+++ lua/entities/npc_vj_rebelradio_hunter/shared.lua
@@ -0,0 +1,8 @@
+ENT.Base 			= "npc_vj_creature_base" -- List of all base types: https://github.com/DrVrej/VJ-Base/wiki/Base-Types
+ENT.Type 			= "ai"
+ENT.PrintName 		= "Hunter"
+ENT.Author 			= "Rebel Radio"
+ENT.Contact 		= "https://steamcommunity.com/groups/RadioGangRebels"
+ENT.Purpose 		= "Spawn it and fight with it!"
+ENT.Instructions 	= "Click on the spawnicon to spawn it."
+ENT.Category		= "HL2 EP2"
